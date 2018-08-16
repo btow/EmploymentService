46e44513d6b7e2c794893b3f210522228c2c894d
bobkov-vv <btow@yandex.ru>
2018-08-16 13:15:05 +0500

    Primary

--- competitor_functional/src/main/java/top/inttech/app/competitor_functional/presentation/presenter/activs/MainPresenter.kt
+++
@@ -1,9 +0,0 @@
-package top.inttech.app.competitor_functional.presentation.presenter.activs
-
-import com.arellomobile.mvp.InjectViewState
-import com.arellomobile.mvp.MvpPresenter
-
-@InjectViewState
-class MainPresenter: MvpPresenter<MainView>() {
-
-}
